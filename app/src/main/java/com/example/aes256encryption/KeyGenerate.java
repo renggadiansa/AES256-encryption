aefd1bffca55ecadf62e83eab3c48f9e021bf17a
renggadiansa <renggadiansa@gmail.com>
2023-07-09 19:20:04 +0700

    solved decrypt

--- app/src/main/java/com/example/aes256encryption/KeyGenerate.java
+++ app/src/main/java/com/example/aes256encryption/KeyGenerate.java
@@ -3,43 +3,34 @@ package com.example.aes256encryption;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
 public class KeyGenerate {
-    private static final int ITERATIONS = 65536;
-    private static final int KEY_LENGTH =256;
-
-    public static String generateKey(String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        byte[] salt = new byte[16];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(salt);
-
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, ITERATIONS, KEY_LENGTH);
-        SecretKey secretKey = secretKeyFactory.generateSecret(pbeKeySpec);
-        byte[] keyBytes = secretKey.getEncoded();
-
-        return bytesToHex(keyBytes);
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder result = new StringBuilder();
-        for (byte b : bytes) {
-            result.append(String.format("%02x", b));
-        }
-        return result.toString();
-    }
+    private static final int KEY_LENGTH = 32;
+    private static final int IV_LENGTH = 16;
 
     public static void main(String[] args) {
-        try {
-            String password = "1234";
-            String encryptionKey = generateKey(password);
-            System.out.println("Encryption Key: " + encryptionKey);
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
+        String aesKey = generateAESKey();
+        String aesIV = generateAESIV();
+        System.out.println("AES Key: " + aesKey);
+        System.out.println("AES IV: " + aesIV);
+    }
+
+    private static String generateAESKey() {
+        byte[] key = new byte[KEY_LENGTH];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(key);
+        return Base64.getEncoder().encodeToString(key);
+    }
+
+    private static String generateAESIV() {
+        byte[] iv = new byte[IV_LENGTH];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        return Base64.getEncoder().encodeToString(iv);
     }
 }
--- app/src/main/java/com/example/aes256encryption/MainActivity.java
+++ app/src/main/java/com/example/aes256encryption/MainActivity.java
@@ -3,34 +3,44 @@ package com.example.aes256encryption;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextInput;
     private Button buttonEncrypt;
     private Button buttonDecrypt;
-    private TextView textViewResult;
+    private TextView textViewEncrypt;
+    private TextView textViewDecrypt;
 
-    private static final String AES_KEY = "YOUR_ENCRYPTION_KEY";
+    private static final String AES_KEY = "YOU_AES_KEY";
+    private static final String AES_IV = "YOU_AES_IV";
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Menghubungkan elemen UI dengan variabel
+
         editTextInput = findViewById(R.id.editTextInput);
         buttonEncrypt = findViewById(R.id.buttonEncrypt);
         buttonDecrypt = findViewById(R.id.buttonDecrypt);
-        textViewResult = findViewById(R.id.textViewResult);
+        textViewEncrypt = findViewById(R.id.textViewEncrypt);
+        textViewDecrypt = findViewById(R.id.textViewDescrypt);
 
         buttonEncrypt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +48,11 @@ public class MainActivity extends AppCompatActivity {
                 String inputText = editTextInput.getText().toString();
 
                 try {
-                    String encryptedText = encrypt(inputText, AES_KEY);
-                    textViewResult.setText("Encrypted Text: " + encryptedText);
+                    String encryptedText = encrypt(inputText, AES_KEY, AES_IV);
+                    textViewEncrypt.setText(encryptedText);
+                    ClipboardManager cm = (ClipboardManager)getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                    cm.setText(textViewEncrypt.getText().toString());
+                    Toast.makeText(getApplicationContext(), "Copied :)", Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -53,8 +66,11 @@ public class MainActivity extends AppCompatActivity {
                 String encryptedText = editTextInput.getText().toString();
 
                 try {
-                    String decryptedText = decrypt(encryptedText, AES_KEY);
-                    textViewResult.setText("Decrypted Text: " + decryptedText);
+                    String decryptedText = decrypt(encryptedText, AES_KEY, AES_IV);
+                    textViewDecrypt.setText(decryptedText);
+                    ClipboardManager cm = (ClipboardManager)getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                    cm.setText(textViewDecrypt.getText().toString());
+                    Toast.makeText(getApplicationContext(), "Copied :)", Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -62,20 +78,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private String encrypt(String inputText, String encryptionKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec keySpec = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
-        byte[] encryptedBytes = cipher.doFinal(inputText.getBytes());
+    private String encrypt(String inputText, String encryptionKey, String initializationVector) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(encryptionKey.getBytes(StandardCharsets.UTF_8), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(initializationVector.getBytes(StandardCharsets.UTF_8));
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        byte[] encryptedBytes = cipher.doFinal(inputText.getBytes(StandardCharsets.UTF_8));
         return Base64.encodeToString(encryptedBytes, Base64.DEFAULT);
     }
 
-    private String decrypt(String encryptedText, String encryptionKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec keySpec = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
-        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+    private String decrypt(String encryptedText, String encryptionKey, String initializationVector) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(encryptionKey.getBytes(StandardCharsets.UTF_8), "AES");
+        IvParameterSpec ivSpec = new IvParameterSpec(initializationVector.getBytes(StandardCharsets.UTF_8));
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
         byte[] encryptedBytes = Base64.decode(encryptedText, Base64.DEFAULT);
         byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-        return new String(decryptedBytes);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
     }
 }
